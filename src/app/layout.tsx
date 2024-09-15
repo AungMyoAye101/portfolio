34dada2d9240547ea5b237564d88b65f1b38e9dc
AungMyoAye <aungmyoaye101@gmail.com>
2024-09-15 19:43:02 +0630

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import { ThemeProvider } from "@/components/Provider";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -25,7 +26,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className=" max-w-7xl mx-auto">{children}</body>
+      <body className=" max-w-8xl mx-auto">
+        <ThemeProvider attribute="class" defaultTheme="dark">
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -6,22 +6,22 @@ const Hero = () => {
   const words = "Transforming Concepts into Seamless User Experiences";
   return (
     <section className="min-h-screen relative flex items-center ">
-      <Spotlight fill="purple" />
-      <div className="h-screen w-full dark:bg-black bg-white  dark:bg-grid-white/[0.4] bg-grid-black/[0.2] flex items-center justify-center absolute top-0 left-0">
+      <Spotlight className="h-screen top-0" fill="white" />
+      <div className="h-screen w-full dark:bg-black bg-white  dark:bg-grid-white/[0.1] bg-grid-black/[0.1] flex items-center justify-center absolute top-0 left-0 -z-10">
         {/* Radial gradient for the container to give a faded look */}
         <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
       </div>
 
       <main className="flex justify-center gap-14 items-center p-8">
-        <div className="flex flex-col gap-2 items-center md:w-[60vw]">
-          <p className="uppercase tracking-widest text-center text-blue-400 ">
+        <div className="flex flex-col gap-1 items-center md:w-[60vw]">
+          <p className="uppercase tracking-widest text-center text-sky-200 ">
             Dynamic Web Magic with Next.js
           </p>
           <TextGenerateEffect
             words={words}
             className="text-center text-[50px] md:text-4xl lg:text-5xl"
           />
-          <p className=" text-center text-xl font-semibold font-serif max-w-[60%]">
+          <p className=" text-center text-sky-200 text-xl font-semibold font-serif max-w-[60%]">
             Hi! I&apos;m Aung Myo Aye, a FullStack Developer based in Myanmar.
           </p>
         </div>
