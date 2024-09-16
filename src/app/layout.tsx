a6e65885b87935d7018cff4a20d7b5852e06e60d
AungMyoAye <aungmyoaye101@gmail.com>
2024-09-16 17:44:31 +0630

    add theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import { ThemeProvider } from "@/components/Provider";
+import { ThemeProvider } from "@/app/Provider";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
