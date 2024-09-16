99b94a8eb8181885cff40081a4e066a051a60ad3
AungMyoAye <aungmyoaye101@gmail.com>
2024-09-16 17:23:09 +0630

    add floating nav

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,19 @@
 import Hero from "@/components/Hero";
+import { FloatingNav } from "@/components/ui/floating-navbar";
+
 import Image from "next/image";
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Testimonials", link: "#testimonials" },
+  { name: "Contact", link: "#contact" },
+];
 
 export default function Home() {
   return (
     <section>
+      <FloatingNav navItems={navItems} />
+      <Hero />
       <Hero />
     </section>
   );
