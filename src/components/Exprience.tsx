ced2b664d833a63a3b08fec4e37b3354a852fe25
AungMyoAye <aungmyoaye101@gmail.com>
2024-09-18 20:35:50 +0630

    add my exprience

--- src/components/Exprience.tsx
+++ src/components/Exprience.tsx
@@ -14,7 +14,7 @@ const Exprience = () => {
         My <span className="text-purple">Work Exprience</span>
       </h1>
 
-      <main className="grid md:grid-cols-2 gap-10 ">
+      <main className="w-full mt-12 grid lg:grid-cols-4 grid-cols-1 gap-10">
         {workExperience.map((item) => (
           <Button
             key={item.id}
@@ -31,8 +31,10 @@ const Exprience = () => {
             }}
             className="flex-1 text-black dark:text-white border-neutral-200 dark:border-slate-800"
           >
-            <div className="flex lg:flex-row flex-col lg:items-center p-3 py-6 md:p-5 lg:p-10 gap-2 relative lg:w-32 md:w-20 w-16">
-              <Image src={item.thumbnail} fill alt={item.title} />
+            <div className="flex lg:flex-row flex-col lg:items-center p-3 py-6 md:p-5 lg:p-10 gap-2 ">
+              <div className="relative lg:w-32 md:w-20 size-16">
+                <Image src={item.thumbnail} fill alt={item.title} />
+              </div>
               <div className="lg:ms-5">
                 <h1 className="text-start text-xl md:text-2xl font-bold">
                   {item.title}
