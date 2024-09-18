cf140742914a83d36d7b4896960c1c0c0f5bf9d9
AungMyoAye <aungmyoaye101@gmail.com>
2024-09-18 10:02:03 +0630

    update recent project

--- src/components/RecentProjects.tsx
+++ src/components/RecentProjects.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { projects } from "@/data";
 import React from "react";
 import { PinContainer } from "./ui/3dPin";
@@ -5,34 +7,42 @@ import { FaLocationArrow } from "react-icons/fa";
 import Image from "next/image";
 const RecentProjects = () => {
   return (
-    <section className="flex flex-col gap-6 py-10 px-6 justify-center items-center">
+    <section
+      className="flex flex-col gap-6 py-10 px-6 justify-center items-center"
+      id="projects"
+    >
       <h1 className="text-xl sm:text-2xl md:3xl lg:text-4xl font-serif">
         A Small Section Of <span className="text-purple">Recent Projects</span>
       </h1>
 
       <main className="flex flex-wrap items-center justify-center p-4 gap-x-16 gap-y-4">
-        {projects.map((project, i) => (
+        {projects.map((item) => (
           <div
-            key={i}
-            className=" lg:min-h-[32.5rem] h-[25rem] flex items-center justify-center sm:w-96 w-[80vw]"
+            className="lg:min-h-[32.5rem] h-[25rem] flex items-center justify-center sm:w-96 w-[80vw]"
+            key={item.id}
           >
-            <PinContainer title={project.link} href={project.link}>
+            <PinContainer
+              title="/ui.aceternity.com"
+              href="https://twitter.com/mannupaaji"
+            >
               <div className="relative flex items-center justify-center sm:w-96 w-[80vw] overflow-hidden h-[20vh] lg:h-[30vh] mb-10">
                 <div
                   className="relative w-full h-full overflow-hidden lg:rounded-3xl"
-                  style={{ background: "url(/bg.png)" }}
+                  style={{ backgroundColor: "#13162D" }}
                 >
-                  <Image
-                    src={project.img}
-                    fill
-                    alt={project.title}
-                    className="object-cover object-center"
-                  />
+                  <img src="/bg.png" alt="bgimg" />
                 </div>
+                <img
+                  src={item.img}
+                  alt="cover"
+                  className="z-10 absolute bottom-0"
+                />
               </div>
+
               <h1 className="font-bold lg:text-2xl md:text-xl text-base line-clamp-1">
-                {project.title}
+                {item.title}
               </h1>
+
               <p
                 className="lg:text-xl lg:font-normal font-light text-sm line-clamp-2"
                 style={{
@@ -40,16 +50,17 @@ const RecentProjects = () => {
                   margin: "1vh 0",
                 }}
               >
-                {project.des}
+                {item.des}
               </p>
+
               <div className="flex items-center justify-between mt-7 mb-3">
                 <div className="flex items-center">
-                  {project.iconLists.map((icon, i) => (
+                  {item.iconLists.map((icon, index) => (
                     <div
-                      key={i}
+                      key={index}
                       className="border border-white/[.2] rounded-full bg-black lg:w-10 lg:h-10 w-8 h-8 flex justify-center items-center"
                       style={{
-                        transform: `translateX(-${5 * i + 2}px)`,
+                        transform: `translateX(-${5 * index + 2}px)`,
                       }}
                     >
                       <img src={icon} alt="icon5" className="p-2" />
