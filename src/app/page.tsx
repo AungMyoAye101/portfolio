de3bbb1e93b63e71d400d18a1f5f9f5f3ba33f73
AungMyoAye <aungmyoaye101@gmail.com>
2024-09-18 20:11:30 +0630

    add exprience components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import Exprience from "@/components/Exprience";
 import GridContainer from "@/components/GridContainer";
 import Hero from "@/components/Hero";
 import RecentProjects from "@/components/RecentProjects";
@@ -18,6 +19,7 @@ export default function Home() {
       <Hero />
       <GridContainer />
       <RecentProjects />
+      <Exprience />
     </section>
   );
 }
---
+++ src/components/Exprience.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Exprience = () => {
+  return <div>Exprience</div>;
+};
+
+export default Exprience;
